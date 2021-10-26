40ed5edeca8f7336712a4cbe1ca582d85fd9534a
Vondyy <83143426+Vondyy@users.noreply.github.com>
2021-10-26 18:39:46 -0400

    Recoded it to get the basics working

--- money_tracker.py
+++ money_tracker.py
@@ -1,11 +1,17 @@
-with open('balance.txt') as f:
-    contents = f.read()
-    print("Your balance is:", contents)
-
-def deposit():
-    deposit = int(input("Enter amount to be deposited: "))
-    new_balance = balance + deposit
-    print("Aasdsadasdmount left in your account: USD" + str(new_balance))
-    return (new_balance)
-balance = deposit()
-##aasdfsdfsdnm
+print("This is where you can keep count of your money")
+with open(r"balance.txt") as f:
+    var = f.read()
+print("Your balance is ", var)
+bob = input("Enter your number here: ")
+with open(r"balance.txt", "w") as f: #open in write mode
+    f.write(bob)
+with open(r"balance.txt") as f:
+    var = f.read()
+print("Your new balance is: ", var)
+# def deposit():
+#     deposit = int(input("Enter amount to be deposited: "))
+#     new_balance = balance + deposit
+#     print("Aasdsadasdmount left in your account: USD" + str(contents))
+#     return (new_balance)
+# balance = deposit()
+# ##aasdfsdfsdnm
