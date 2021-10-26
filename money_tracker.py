ae36874d46884a583977d1093f41743bca2fd09e
Vondyy <83143426+Vondyy@users.noreply.github.com>
2021-10-26 12:02:19 -0400

    Add deposit system

--- money_tracker.py
+++ money_tracker.py
@@ -1,3 +1,10 @@
 print("This is where you can keep count of your money")
 balance = 0
 print("Your balance is ", balance)
+
+def deposit():
+    deposit = int(input("Enter amount to be deposited: "))
+    new_balance = balance + deposit
+    print("Amount left in your account: USD" + str(new_balance))
+    return (new_balance)
+balance = deposit()
